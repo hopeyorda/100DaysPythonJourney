71bbddb85168f091dc93039874dcb7440ddfc9c6
Yordanos Shaba <yordanosshaba@gmail.com>
2025-01-29 10:27:28 +0300

    This commit is about Mathematical operation and precedence

--- Day_2/interactive_excersice_1.py
+++ Day_2/interactive_excersice_1.py
@@ -2,5 +2,5 @@
 weight = float(input("What is your weight(in kg)? \n"))
 height = float(input("What is your height(in m)? \n"))
 
-BMI = weight/pow(height,2)
+BMI = weight/ height ** 2
 print(int(BMI))
