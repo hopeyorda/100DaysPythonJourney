842bed31cf5b3e7b23b1f3d87b3e73d2fd2aeb84
Yordanos Shaba <yordanosshaba@gmail.com>
2025-01-30 10:12:41 +0300

    This commit is Fourth commit about BMI exercise of day 3

---
+++ Day_3/BMI_excersice_2.py
@@ -0,0 +1,19 @@
+weight = float(input("What is your weight? \n"))
+height = float(input("what is your height? \n"))
+
+bmi = round(weight / height ** 2)
+if bmi < 18.5 :
+    print(f"Your BMI is {bmi}")
+    print("Your are Underweight Bro. Eat much food!")
+elif 18.5 < bmi < 25 :
+    print(f"Your BMI is {bmi}")
+    print("You are are at Normal state. Keep shinning bro!")
+elif 25 < bmi < 30 :
+    print(f"Your BMI is {bmi}")
+    print("You are slightly overweight. So bro look at your self!")
+elif 30 < bmi < 35 :
+    print(f"Your BMI is {bmi}")
+    print("Your are obese.Make exercise.")
+else :
+    print(f"Your BMI is {bmi}")
+    print("You are clinically obese. Bro you are on risk.")
