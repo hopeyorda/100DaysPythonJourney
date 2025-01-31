45add1e8aef6c0821b92b8c7058270c655cac5bb
Yordanos Shaba <yordanosshaba@gmail.com>
2025-01-31 08:38:09 +0300

    This commit is the first commit about random module  of day 4

---
+++ Day_4/random_module.py
@@ -0,0 +1,8 @@
+import random
+#Generate Random integer
+random_integer = random.randint(1,10)
+print(random_integer)
+
+#Generate Random Floating point
+random_float = random.uniform(0.0 , 5.0)
+print(random_float)
