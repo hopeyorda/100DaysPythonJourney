2a344df5bad3cb9316cb446427b1fec5ad1dff47
Yordanos Shaba <yordanosshaba@gmail.com>
2025-01-31 11:22:21 +0300

    This commit is the fourth commit about who's playing the bill   of day 4

---
+++ Day_4/who's_playing_exercise.py
@@ -0,0 +1,10 @@
+import random
+
+name =input("Give me everybody's names, separated by "
+       "comma : ")
+name1 = name.split(", ")
+name2 = len(name1)
+
+random_choice = random.randint(0,name2-1)
+
+print(name1[random_choice] + " is pay the bill.")
